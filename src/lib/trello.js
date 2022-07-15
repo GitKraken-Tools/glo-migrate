1f7b7e4b8a5fcea6bf45e716b3f6b2064c3267ec
Justin White <kyjus25@gmail.com>
2022-07-14 20:31:28 -0500

    Create lists in order

--- src/lib/trello.js
+++ src/lib/trello.js
@@ -4,7 +4,12 @@ export const trello = (creator) => {
     const tokens = creator.tokens.find(i => i.type === 'Trello').token;
     return {
         board: (name) => new Promise((resolve, reject) => {
-            Auth(null, creator.gitkrakenId).getProtectedResource(`https://api.trello.com/1/boards?name=${name}`, "POST", tokens.accessToken, tokens.accessTokenSecret, function (error, data, response) {
+            Auth(null, creator.gitkrakenId).getProtectedResource(`https://api.trello.com/1/boards?name=${name}&defaultLists=false&defaultLabels=false&desc=Migrated from GitKraken Glo`, "POST", tokens.accessToken, tokens.accessTokenSecret, function (error, data, response) {
+                resolve(JSON.parse(data));
+            });
+        }),
+        list: (boardId, name, index) => new Promise((resolve, reject) => {
+            Auth(null, creator.gitkrakenId).getProtectedResource(`https://api.trello.com/1/boards/${boardId}/lists?name=${name}`, "POST", tokens.accessToken, tokens.accessTokenSecret, function (error, data, response) {
                 resolve(JSON.parse(data));
             });
         })
--- src/routes/api/trello/migrate.js
+++ src/routes/api/trello/migrate.js
@@ -8,10 +8,19 @@ export const post = async (event) => {
     const gitkrakenBoard = await gk(creator.tokens.find(i => i.type === 'GitKraken').token).board(session.gitkrakenBoardId);
     const gitkrakenCards = await gk(creator.tokens.find(i => i.type === 'GitKraken').token).cards(session.gitkrakenBoardId);
 
+    
     const trelloBoard = await trello(creator).board(session.gitkrakenBoardName);
-    console.log('TRELLO BOARD', trelloBoard);
-    console.log('should migrate', session);
-    console.log('board', gitkrakenBoard, gitkrakenCards);
+
+    for (const list of gitkrakenBoard.columns.reverse()) {
+        await trello(creator).list(trelloBoard.id, list.name);
+    }
+
+    console.log('LISTS', lists);
+    const trelloLists = await Promise.all(lists).then(i => console.log(i));
+
+    // console.log('TRELLO BOARD', trelloBoard);
+    // console.log('should migrate', session);
+    // console.log('board', gitkrakenBoard, gitkrakenCards);
     
     return {}
 }
